b58ac9aed3fe8dbc1a7084c49ba2bcb92b01e88c
KULDEEP <kuldeepdhangarkd@gmail.com>
2023-05-22 09:08:29 +0530

    Convert dict literal to dict constructor

--- item/views.py
+++ item/views.py
@@ -5,7 +5,4 @@ from .models import Item
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
     related_item = Item.objects.filter(category=item.Category, is_sold=False).exclude(pk=pk)[0:3]
-    return render(request, 'item/detail.html', {
-        'item': item,
-        'related_item': related_item,
-    })
+    return render(request, 'item/detail.html', dict(item=item, related_item=related_item))
