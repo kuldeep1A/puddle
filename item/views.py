e6a35109ac25fe9da6920956619d6d63ecb66c4c
kuldeep1a <kuldeepdhangarkd@gmail.com>
2023-05-22 14:36:22 +0530

    change

--- item/views.py
+++ item/views.py
@@ -4,5 +4,5 @@ from .models import Item
 
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
-    related_item = Item.objects.filter(Category=item.Category, is_sold=False).exclude(pk=pk)[0:3]
-    return render(request, 'item/detail.html', dict(item=item, related_item=related_item))
+    related_items = Item.objects.filter(Category=item.Category, is_sold=False).exclude(pk=pk)[0:3]
+    return render(request, 'item/detail.html', dict(item=item, related_items=related_items))
