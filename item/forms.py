f24beed2a3d5c7c40a29907c61ff1c360a37268f
KULDEEP <kuldeepdhangarkd@gmail.com>
2023-05-23 19:34:07 +0530

    complete the item add forma and add chair

--- item/forms.py
+++ item/forms.py
@@ -1,2 +1,18 @@
 from django import forms
-from .
+from .models import Item
+
+INPUT_CLASSES = 'w-full py-4 px-6 rounded-xl border'
+
+
+class NewItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields = ('Category', 'name', 'description', 'price', 'image')
+
+        widgets = dict(
+            Category=forms.Select(attrs={'class': INPUT_CLASSES}),
+            name=forms.TextInput(attrs={'class': INPUT_CLASSES}),
+            description=forms.Textarea(attrs={'class': INPUT_CLASSES}),
+            price=forms.TextInput(attrs={'class': INPUT_CLASSES}),
+            image=forms.FileInput(attrs={'class': INPUT_CLASSES}),
+        )
