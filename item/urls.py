c2f12ddfd002a1ab18ab8197d945a380af63f808
KULDEEP <kuldeepdhangarkd@gmail.com>
2023-05-23 19:34:07 +0530

    complete the item add forma and add chair

--- item/urls.py
+++ item/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 app_name = 'item'
 
 urlpatterns = [
-    path('<int:pk>/', views.detail, name='detail')
+    path('<int:pk>/', views.detail, name='detail'),
+    path('new/', views.new, name='new')
 ]
