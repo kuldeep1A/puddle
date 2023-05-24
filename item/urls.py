d7462069c41ad86a0e06fc19dcbf36b7d62a7f8b
KULDEEP <kuldeepdhangarkd@gmail.com>
2023-05-24 14:37:32 +0530

    added item

--- item/urls.py
+++ item/urls.py
@@ -6,5 +6,6 @@ app_name = 'item'
 urlpatterns = [
     path('<int:pk>/', views.detail, name='detail'),
     path('<int:pk>/delete/', views.delete, name='delete'),
+    path('<int:pk>/edit/', views.delete, name='edit'),
     path('new/', views.new, name='new')
 ]
